97e8c5de243749de305da6ab0d8a9a113e57f3a5
Lilith <ll.file.top.secret@yandex.ru>
2022-11-23 21:48:28 +0300

    add Example_05_ConditionIfElse

---
+++ Example_05_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ведите ваше имя ");
+string username = Console.ReadLine();
+
+//if(username == "Маша")
+
+if(username.ToLower() == "маша") // без учета регистра
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
