c898e6c55dccaec9749526ed2311021229c55442
Lilith <ll.file.top.secret@yandex.ru>
2022-11-23 21:37:27 +0300

    add another method Sum

--- Example_03_Sum/Program.cs
+++ Example_03_Sum/Program.cs
@@ -1,6 +1,12 @@
-﻿int numberA = 6584;
-int numberB = 464654;
+﻿//int numberA = 6584;
+//int numberB = 464654;
 
+int numberA = new Random().Next(1, 10); //1,2,3...9
+Console.Write(numberA);
+Console.Write(" + ");
+int numberB = new Random().Next(1, 10);
+Console.Write(numberB);
+Console.Write(" = ");
 int result = numberA + numberB;
 
 Console.WriteLine(result);
