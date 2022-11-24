87ee39b149f36742181566700cce56240a6286e3
Lilith <ll.file.top.secret@yandex.ru>
2022-11-24 19:51:36 +0300

    add Example_10_MethodArray

---
+++ Example_10_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {19, 42, 37, 43, 8, 52, 67, 72, 8};
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;                  //находит только первый элемент
+    } 
+    index++;
+}
